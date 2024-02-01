a4ac3aa49788c203e91e562208229b334a285d9f
Cardano Blockhouse [CBH] <90690011+cardano-blockhouse@users.noreply.github.com>
2024-02-01 12:35:45 +0100

    API error handling added

--- src/Http/Controllers/PaymentGatewayController.php
+++ src/Http/Controllers/PaymentGatewayController.php
@@ -11,18 +11,40 @@ class PaymentGatewayController extends Controller
     public function setNewPayment(Request $request) {
         $paymentUUID = $request->uuid;
         $walletReceiver = $request->wallet_receiver;
-        $walletsender = $request->wallet_sender;
+        $walletSender = $request->wallet_sender;
         $after_blockheight = $request->after_blockheight;
         $token_amount = $request->token_amount;
         $token_policy_id = $request->token_policy_id;
         $token_name_hex = $request->token_name_hex;
+
+        $error_message = '';
+
+        if (!isset($paymentUUID)) {
+            $error_message .= 'uuid can not be null';
+        }
+        if (!isset($walletReceiver)) {
+            $error_message .= 'Wallet receiver address can not be null';
+        }
+        if (!isset($walletSender)) {
+            $error_message .= 'Wallet sender address can not be null';
+        }
+        if (!isset($after_blockheight)) {
+            $error_message .= 'After blockheight can not be null';
+        }
+        if (!isset($token_amount)) {
+            $error_message .= 'Token amount can not be null';
+        }
+
+        if ($error_message != '') {
+            return array('status' => $error_message);
+        }
 
         $newPayment = IamxUserPayment::updateOrCreate(
             [
                 'payment_uuid' => $paymentUUID
             ],[
                 'wallet_receiver' => $walletReceiver,
-                'wallet_sender' => $walletsender,
+                'wallet_sender' => $walletSender,
                 'after_blockheight' => $after_blockheight,
                 'token_amount' => $token_amount,
                 'token_policy' => $token_policy_id,
@@ -31,15 +53,25 @@ class PaymentGatewayController extends Controller
         );
 
         if ($newPayment) {
-            return array('status' => 'Payment UUID has been inserted to the database');
+            return array('status' => 'Payment UUID has been inserted to the database.');
         } else {
-            return array('status' => 'Something went wrong while inserting the new row to the database');
+            return array('status' => 'Something went wrong while inserting the new row to the database.');
         }
 
     }
 
     public function checkPayment(Request $request) {
         $paymentUUID = $request->uuid;
+
+        $error_message = '';
+
+        if (!isset($paymentUUID)) {
+            $error_message .= 'uuid can not be null';
+        }
+
+        if ($error_message != '') {
+            return array('status' => $error_message);
+        }
 
         $userpayment = DB::table('iamx_user_payments')
             ->select('is_paid', 'tx_id')
